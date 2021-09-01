7aa3d8271c5ce7e257a83f396c0b23780c9fac2e
raffael marinho <silvamarinho10@gmail.com>
2021-09-01 11:12:11 -0300

    feat: 1,2,5,6,7

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,23 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  console.log((base * height) / 2);
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let tell = 0;
+  for (let i = 0; i < string; i++) {
+    
+  }
 }
 
 // Desafio 4
@@ -19,18 +26,39 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let spots = wins * 3 + ties * 1;
+  return spots;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  let bigger = arr[0];
+  let repeat = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > bigger) {
+      bigger = arr[i];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === bigger) {
+      repeat++;
+    }
+  }
+  return repeat;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let calc1 = mouse - cat1;
+  let calc2 = mouse - cat2;
+  if (calc1 > calc2) {
+    return cat2;
+  } else if (calc2 > calc1) {
+    return cat1;
+  } else if (calc1 === calc2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
